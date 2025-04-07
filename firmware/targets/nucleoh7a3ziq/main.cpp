5f1dbba4ece397d92c5d281a95bafa964eab84d0
unknown <m1pearson@me.com>
2025-04-07 21:54:06 +0100

    Add stm32_hal code and example blinky with stm32HAL

--- firmware/targets/nucleoh7a3ziq/main.cpp
+++ firmware/targets/nucleoh7a3ziq/main.cpp
@@ -1,4 +1,5 @@
 #include "stm32h7xx.h"
+#include "stm32h7xx_hal.h"
 #include <stddef.h>
 
 static inline void NO_OP()
@@ -23,8 +24,21 @@ int main()
   // Set PA6 to general purpose output mode
   MODIFY_REG(GPIOB->MODER, GPIO_MODER_MODE0, GPIO_MODER_MODE0_0);
 
+  // Setup red LED using HAL PB14
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  GPIO_InitStruct.Pin = GPIO_PIN_14;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
   while (true)
   {
+    // Toggle Other LED
+    HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_14);
+
     // Toggle User LED
     const uint32_t bit_value = READ_BIT(GPIOB->ODR, GPIO_ODR_OD0);
     const bool bit_set = (bit_value == GPIO_ODR_OD0);
