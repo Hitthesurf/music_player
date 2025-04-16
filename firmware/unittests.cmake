1c5957f5a124e4ac2deaee1ebb7b4b344c467a3e
unknown <m1pearson@me.com>
2025-04-16 23:04:44 +0100

    Implement cmake preset to build and run unit test with clang tidy checks on windows

--- firmware/unittests.cmake
+++ firmware/unittests.cmake
@@ -8,6 +8,11 @@ fetchcontent_declare(
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 fetchcontent_makeavailable(googletest)
 
+# Has to be enabled after fetching gtest, otherwise runs on gtest
+if (CLANG_TIDY)
+  set(CMAKE_C_CLANG_TIDY "clang-tidy;--allow-no-checks" CACHE STRING "Enable clang-tidy for C")
+  set(CMAKE_CXX_CLANG_TIDY "clang-tidy;--allow-no-checks" CACHE STRING "Enable clang-tidy for C++")
+endif()
 
 add_subdirectory(firmware/music_player)
 
